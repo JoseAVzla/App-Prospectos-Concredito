9c59061694df36f792347f5c896e212fed6602f1
josevalenzuela <pepe13.2.25@gmail.com>
2020-09-18 18:54:17 -0600

    botones enviar y cancelar agregados

--- app/src/main/java/com/josevalenzuela/prospectosconcreditoapp/View/AgegarProspectoFragmet.java
+++ app/src/main/java/com/josevalenzuela/prospectosconcreditoapp/View/AgegarProspectoFragmet.java
@@ -1,17 +1,11 @@
 package com.josevalenzuela.prospectosconcreditoapp.View;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.provider.MediaStore;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +13,12 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.josevalenzuela.prospectosconcreditoapp.Presenter.AgergarProspectoPresenter;
 import com.josevalenzuela.prospectosconcreditoapp.R;
@@ -26,25 +26,26 @@ import com.josevalenzuela.prospectosconcreditoapp.adapters.NuevoDocumentoAdapter
 import com.josevalenzuela.prospectosconcreditoapp.contracts.AgregarProspectoContract;
 import com.josevalenzuela.prospectosconcreditoapp.models.Prospecto;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class AgegarProspectoFragmet extends Fragment implements AgregarProspectoContract.View {
 
-    private ImageView imageView;
-    private int docIndex = 0;
-    private Uri img_uri;
-    private List<Bitmap> bitmaps;
+    private ImageView agregarDocumento;
+    private List<Bitmap> documentosBitmap;
     private List<String> docsEncodedList;
     private AgregarProspectoContract.Presenter presenter;
+
     private Fragment lsitadoFragment;
+    private AlertDialog.Builder alertBuilder;
+
     //componentes iterfaz
     private TextInputEditText nombreEditTxt, primerAppEditTxt, segundoAppEditTxt, telefonoEditTxt, rfcEditTxt, coloniaEditTxt, calleEditTxt, numeroEditTxt, codigoPostalEditTxt;
     private RecyclerView recyclerView;
     private LinearLayout layoutAddDoc;
-    private ImageView enviarBtn;
+    private FloatingActionButton enviarBtn, cancelarBtn;
+
     private final int REQUEST_CODE = 1;
 
 
@@ -58,10 +59,13 @@ public class AgegarProspectoFragmet extends Fragment implements AgregarProspecto
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_agegar_prospecto, container, false);
-        bitmaps = new ArrayList<>();
+        documentosBitmap = new ArrayList<>();
         docsEncodedList = new ArrayList<>();
         recyclerView = view.findViewById(R.id.recyclerViewAgregarDocs);
-        imageView = view.findViewById(R.id.agregarDocId);
+        agregarDocumento = view.findViewById(R.id.agregarDocId);
+        enviarBtn = view.findViewById(R.id.guardarBtnId);
+        cancelarBtn = view.findViewById(R.id.cancelarBtnId);
+        alertBuilder = new AlertDialog.Builder(getActivity());
 
         nombreEditTxt = view.findViewById(R.id.nuevoProspectoNombreId);
         primerAppEditTxt = view.findViewById(R.id.nuevoProspectoAppPaternoETxtId);
@@ -72,7 +76,7 @@ public class AgegarProspectoFragmet extends Fragment implements AgregarProspecto
         coloniaEditTxt = view.findViewById(R.id.nuevoProspectoColoniaId);
         codigoPostalEditTxt = view.findViewById(R.id.nuevoProspectoCodPostalId);
         numeroEditTxt = view.findViewById(R.id.nuevoProspectoNumeroCasaId);
-        enviarBtn = view.findViewById(R.id.saveProspectoId);
+        enviarBtn = view.findViewById(R.id.guardarBtnId);
         layoutAddDoc = view.findViewById(R.id.recyclerProspectoViewId);
 
         presenter = new AgergarProspectoPresenter(this);
@@ -83,35 +87,62 @@ public class AgegarProspectoFragmet extends Fragment implements AgregarProspecto
         enviarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                    presenter.agregarProspecto(bitmaps, nombreEditTxt.getText().toString(), primerAppEditTxt.getText().toString(), segundoAppEditTxt.getText().toString(),
+                if (presenter.validarFormulario(nombreEditTxt.getText().toString(), primerAppEditTxt.getText().toString(), segundoAppEditTxt.getText().toString(),
+                        calleEditTxt.getText().toString(), numeroEditTxt.getText().toString(), coloniaEditTxt.getText().toString(),
+                        codigoPostalEditTxt.getText().toString(), telefonoEditTxt.getText().toString(), rfcEditTxt.getText().toString())) {
+                    presenter.agregarProspecto(documentosBitmap, nombreEditTxt.getText().toString(), primerAppEditTxt.getText().toString(), segundoAppEditTxt.getText().toString(),
                             calleEditTxt.getText().toString(), numeroEditTxt.getText().toString(), coloniaEditTxt.getText().toString(),
                             codigoPostalEditTxt.getText().toString(), telefonoEditTxt.getText().toString(), rfcEditTxt.getText().toString());
+
+                }
 
 
             }
         });
 
 
+        cancelarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                alertBuilder.setMessage(R.string.cancelarDialogMensaje).setTitle(R.string.cancelarDialogTitulo)
+                        .setCancelable(false)
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                getActivity().getSupportFragmentManager().beginTransaction().addToBackStack(null)
+                                        .replace(R.id.contenedorFragmentos, lsitadoFragment).commit();
+                            }
+                        })
+                        .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = alertBuilder.create();
+                alertDialog.show();
+
+            }
+        });
 
 
         //Agregar documento
-        imageView.setOnClickListener(new View.OnClickListener() {
+        agregarDocumento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageView.setColorFilter(getResources().getColor(R.color.colorAccent));
+                agregarDocumento.setColorFilter(getResources().getColor(R.color.colorAccent));
                 Intent camara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (camara.resolveActivity(getContext().getPackageManager()) != null) {
                     startActivityForResult(camara, REQUEST_CODE);
                 }
-                docIndex++;
             }
         });
         return view;
     }
 
-    private void addDoc(){
-        NuevoDocumentoAdapter adapter = new NuevoDocumentoAdapter(bitmaps);
+    private void mostrarDocumentoCapturado() {
+        NuevoDocumentoAdapter adapter = new NuevoDocumentoAdapter(documentosBitmap);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
     }
@@ -120,18 +151,12 @@ public class AgegarProspectoFragmet extends Fragment implements AgregarProspecto
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
 
-        if (REQUEST_CODE == requestCode){
+        if (REQUEST_CODE == requestCode) {
             assert data != null;
             Bitmap photo = (Bitmap) data.getExtras().get("data");
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            photo.compress(Bitmap.CompressFormat.PNG,40,bos);
-            byte[] bb = bos.toByteArray();
-            String encode = Base64.encodeToString(bb, Base64.DEFAULT);
-            docsEncodedList.add(encode);
-            bitmaps.add(photo);
+            documentosBitmap.add(photo);
         }
-
-        addDoc();
+        mostrarDocumentoCapturado();
     }
 
     @Override
